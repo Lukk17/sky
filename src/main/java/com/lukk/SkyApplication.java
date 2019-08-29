2da51d19e20a6f24d63a4b284715fd7bb545e163
Lukk17 <luksarna@gmail.com>
2019-08-29 17:57:11 +0200

    log test

--- src/main/java/com/lukk/SkyApplication.java
+++ src/main/java/com/lukk/SkyApplication.java
@@ -17,7 +17,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkyApplication {
 
 	public static void main(String[] args) {
-
+		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> App start <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 		SpringApplication.run(SkyApplication.class, args);
 	}
 
